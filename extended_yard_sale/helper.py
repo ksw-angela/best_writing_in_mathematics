de8dae81a6553f446cdd06e2a00bb677869a7df9
angela.kang <angela.kang@loblaw.ca>
2022-06-23 16:41:45 -0400

    style: update docstrings

--- extended_yard_sale/helper.py
+++ extended_yard_sale/helper.py
@@ -38,13 +38,55 @@ class Wealth:
 
 
 class ExtendedYardSale(Wealth):
+    """Conducts the extended yard sale for a given wealth dictionary.
+
+        Attributes:
+            wealth (dict): Wealth of all transacting parties.
+            win_percentage (float): Percentage of the poorer person's wealth that is moved from the
+                loser of the transaction to the winner.
+            n (int): Number of transactions. Defaults to 10000.
+            chi (float): Tax rate paid before each transaction by people who's wealth exceeds the
+                average wealth of the system and is distributed equally to people who's wealth
+                is below the average wealth of the system. Defaults to 0, or no tax being paid.
+            zeta (float): Proportional to the amount we bias the coin flip in favour of the richer
+                person. The bias is equal to:
+                    zeta * (absolute difference in wealth of the transacting parties)
+                Defaults to 0, or no bias for the richer person.
+            kappa (float): Proportional to the amount of negative wealth a person can have.
+                The wealth of the poorest person at any time is -S, where:
+                    S = kappa * average wealth
+                Before each transaction, we loan S to both agents so both have positive wealth.
+                After the transaction is complete, both parties repay their debt of S.
+
+    """
     def __init__(self,
                  start_wealth,
-                 win_percentage: float,
+                 win_percentage,
                  n=10000,
                  chi=0,
                  zeta=0,
                  kappa=0):
+        """Initializes the extended yard sale model.
+
+        Args:
+            start_wealth (dict): Wealth of all transacting parties.
+            win_percentage (float): Percentage of the poorer person's wealth that is moved from the
+                loser of the transaction to the winner.
+            n (int): Number of transactions. Defaults to 10000.
+            chi (float): Tax rate paid before each transaction by people who's wealth exceeds the
+                average wealth of the system and is distributed equally to people who's wealth
+                is below the average wealth of the system. Defaults to 0, or no tax being paid.
+            zeta (float): Proportional to the amount we bias the coin flip in favour of the richer
+                person. The bias is equal to:
+                    zeta * (absolute difference in wealth of the transacting parties)
+                Defaults to 0, or no bias for the richer person.
+            kappa (float): Proportional to the amount of negative wealth a person can have.
+                The wealth of the poorest person at any time is -S, where:
+                    S = kappa * average wealth
+                Before each transaction, we loan S to both agents so both have positive wealth.
+                After the transaction is complete, both parties repay their debt of S.
+
+        """
         super().__init__(start_wealth)
         self.win_percentage = win_percentage
         self.n = n
@@ -58,8 +100,11 @@ class ExtendedYardSale(Wealth):
         self._avg_wealth = self.wealth['wealth'].mean()
 
     def _pay_tax(self):
-        # people that are wealthier than the average pay the flat wealth tax
-        # tax is distributed equally among people that are poorer than the average
+        """Updates the wealth attribute in the following way:
+        - People richer than the average wealth pay a flat wealth tax proportional
+          to the chi attribute
+        - People poorer than the average equally share the tax paid by the richer people
+        """
         tax = self.chi * self.wealth.loc[self.wealth['wealth'] > self._avg_wealth, 'wealth'].sum()
         per_person_subsidy = round(
             tax / len(self.wealth.loc[self.wealth['wealth'] <= self._avg_wealth].index))
@@ -67,24 +112,40 @@ class ExtendedYardSale(Wealth):
         self.wealth.loc[self.wealth['wealth'] <= self._avg_wealth, 'wealth'] += per_person_subsidy
 
     def _loan_S(self):
-        # Loan S to everyone before the transaction
+        """Updates the wealth attribute for all people by providing a loan proportional to
+        the attribute kappa before the transaction.
+        """
         self.wealth.loc[:, 'wealth'] = self.wealth.loc[:, 'wealth'] + (
                     self.kappa * self._avg_wealth)
 
     def _collect_S(self):
-        # Both parties pay back loan of S at the end of transaction
+        """Updates the wealth attribute for all people by paying back the loan after
+        the transaction.
+        """
         self.wealth.loc[:, 'wealth'] = self.wealth.loc[:, 'wealth'] - (
                     self.kappa * self._avg_wealth)
 
     def _add_pair_id(self):
-        # match people by randomly shuffling people add a column 'group' to represent the pair id
+        """Returns a dictionary of dictionaries with transacting members of form:
+            {pair_id: {person_id: wealth}}
+        The pair_id is shared among transacting pairs. For each pair_id, there should only be two
+        person_id's.
+        :return: object
+        """
         wealth_permutation = self.wealth.sample(frac=1).reset_index(drop=True)
         wealth_permutation['group'] = list(range(int(self._n_people / 2))) * 2
         return wealth_permutation
 
     def _add_dice_roll(self, wealth_permutation):
-        # Roll dice for all pairs using the bias that gives the wealthier person the advantage
-        # Let 0 be a win for the richer person and 1 be a win for the poorer person
+        """Determine the winner of the transaction for all pairs.
+
+        Include the bias that gives the wealthier person the advantage.
+        Let 0 be a win for the richer person and 1 for the poorer person.
+
+        :param wealth_permutation: dictionary of dictionaries of form:
+            {pair_id: {person_id: wealth}}
+        :return: object
+        """
         wealth_difference = (wealth_permutation.groupby('group')['wealth'].max()
                              - wealth_permutation.groupby('group')['wealth'].min()).reset_index(
             drop=True)
@@ -98,7 +159,12 @@ class ExtendedYardSale(Wealth):
         return wealth_permutation
 
     def _exchange_wealth(self, wealth_permutation):
-        # Move money from the loser to the winner
+        """Return the updated wealth with money moving from the loser to the winner.
+
+        :param wealth_permutation: dictionary of dictionaries of form:
+            {pair_id: {person_id: wealth}}
+        :return: object
+        """
         exchange_amount = self.win_percentage * wealth_permutation.groupby('group')['wealth'] \
             .min().reset_index(drop=True)
         poor = wealth_permutation.sort_values(['group', 'wealth']) \
@@ -112,6 +178,10 @@ class ExtendedYardSale(Wealth):
         return wealth_permutation
 
     def perform_sale(self):
+        """Runs a single iteration of the extended yard sale model and updates the wealth attribute.
+
+        :return: None
+        """
         self._update_average_wealth()
         self._pay_tax()
         self._loan_S()
@@ -122,6 +192,13 @@ class ExtendedYardSale(Wealth):
         self._collect_S()
 
     def run_yard_sale(self, plot_n=1000, plot=True):
+        """Runs multiple iterations of the extended yard sale model.
+
+        :param plot_n: (int) The number of iterations that need to elapse before plotting the wealth
+            distribution. Default to 1000.
+        :param plot: (booelan) If True, plots are returned. Default to True.
+        :return: None
+        """
         if plot & (plot_n > self.n):
             raise Exception('plot_n needs to be lower than n')
         for i in tqdm(range(self.n)):
